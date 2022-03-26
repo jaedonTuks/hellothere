d2c1c186bd892b36ce351f2f73481655079611ec
Jaedon Heger <jaedon.heger@labs.epiuse.com>
2022-03-26 22:30:12 +0200

    Webpage shell setup
    - adding basic theming and components for frontend
    
    Issue: #4

---
+++ client/src/AppHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app-bar
+      app
+      color="accent"
+      dark
+  >
+    <div class="d-flex align-center">
+     <h1 class="logo">Hello There!</h1>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+        text
+    >
+      <span class="mr-2">Home</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+
+    <v-btn
+        text
+    >
+      <span class="mr-2">Profile</span>
+      <v-icon>mdi-profile</v-icon>
+    </v-btn>
+    <v-btn
+        text
+    >
+      <span class="mr-2">Leaderboards</span>
+      <v-icon>mdi-profile</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+.logo {
+  color: var(--v-secondary-base);
+}
+
+span {
+  color: var(--v-info-base);
+}
+</style>
--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,22 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    dark: false,
+    themes: {
+      light: {
+        primary: '#80DED9',
+        secondary: '#C4676A',
+        info: '#FFFBFE',
+        accent: '#465362',
+        error: '#ef5350',
+      },
+    },
+  },
 });
